d13f871aa3a5067cdfd08e27862c181c31682fc9
Rowan Laffeber <9026051@student.zadkine.nl>
2025-02-19 08:42:32 +0100

    created login page
    created a login page, no styling yet, just $_Post

---
+++ login.php
@@ -0,0 +1,7 @@
+<form action="loginaction.php" method="post">
+            <label for="Email">Email</label>
+                <input type="email" name="Email" id="email" placeholder="Email" required >
+            <label for="Subject">Subject</label>
+                <input type="password" name="password" id="password" placeholder="password">
+            <input type="submit" name="submit" id="submit">
+        </form>
---
+++ loginaction.php
@@ -0,0 +1,4 @@
+<?php 
+    echo $_POST["Email"]; 
+    echo $_POST["password"];
+?>
