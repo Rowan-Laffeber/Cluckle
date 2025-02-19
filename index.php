fe988f10ed5c11187d004d4630b5bedfac6daa04
Rowan Laffeber <9026051@student.zadkine.nl>
2025-02-19 22:00:34 +0100

    added homepage
    added a homepage with navbar and mock posts

--- index.php
+++ index.php
@@ -1,1 +1,276 @@
-<?php echo	"hello world, this a test" ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="description" content="portfolio">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=2.0">
+    <title>Rowan Laffeber</title>
+    <link rel="stylesheet" href="assets/styles/main.css">
+    <!-- <link id="color_mode" rel="stylesheet" href="assets/cluckle-darkmode.css"> -->
+  </head>
+  <body>
+    <?php    $url =  $_SERVER['REQUEST_URI'];?>
+    <nav class="desktop-nav">
+      <ul>
+        <img src="assets/img/chicken-line-white.png" alt="">
+
+        <li><a <?php if ($url === '/chirpify/index.php'){ echo "class='active'";} ?> href="index.php">Home</a></li>
+        <li <?php if ($url === '/chirpify/#'){ echo "class='active'";} ?>><a href="#">Explore</a></li>
+        <li <?php if ($url === '/chirpify/#'){ echo "class='active'";} ?>><a href="#">Notifications</a></li>
+        <li <?php if ($url === '/chirpify/#'){ echo "class='active'";} ?>><a href="#">Messages</a></li>
+        <li <?php if ($url === '/chirpify/#'){ echo "class='active'";} ?>><a href="#">Bookmarks</a></li>
+        <li <?php if ($url === '/chirpify/#'){ echo "class='active'";} ?>><a href="login.php">Login</a></li>
+        <li <?php if ($url === '/chirpify/#'){ echo "class='active'";} ?>><a href="#">Profile</a></li>
+        <li <?php if ($url === '/chirpify/#'){ echo "class='active'";} ?>><a href="#">Premium</a></li>
+        <li <?php if ($url === '/chirpify/#'){ echo "class='active'";} ?>><a href="#">More</a></li>
+      </ul>
+    </nav>
+    <div class="articles">
+    <article>
+      <img src="assets/img/chicken-solid-white.png" alt="profilePicture">
+      <div class="userAndContent">
+        <div class="user">
+          <p class="username">De Kale Kip</p>
+          <p class="tag">@DeKaleKip</p>
+          <p class="timePosted">&middot; 12-03-24</p>
+        </div>
+        <div class="content">
+          <p>lorem</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>bdgfbgfeht</p>
+        </div>
+        <div class="analytics">
+          <ul>
+            <li><a href="#">reactions 12</a></li>
+            <li><a href="#">shares 5</a></li>
+            <li><a href="#">likes 32</a></li>
+            <li><a href="#">views 103</a></li>
+            <li><a href="#">archive</a></li>
+          </ul>
+        </div>
+      </div>  
+    </article>
+
+    <article>
+      <img src="assets/img/chicken-solid-white.png" alt="profilePicture">
+      <div class="userAndContent">
+        <div class="user">
+          <p class="username">De Kale Kip</p>
+          <p class="tag">@DeKaleKip</p>
+          <p class="timePosted">&middot; 12-03-24</p>
+        </div>
+        <div class="content">
+          <p>lorem</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>bdgfbgfeht</p>
+        </div>
+        <div class="analytics">
+          <ul>
+            <li><a href="#">reactions 12</a></li>
+            <li><a href="#">shares 5</a></li>
+            <li><a href="#">likes 32</a></li>
+            <li><a href="#">views 103</a></li>
+            <li><a href="#">archive</a></li>
+          </ul>
+        </div>
+      </div>  
+    </article>
+
+    <article>
+      <img src="assets/img/chicken-solid-white.png" alt="profilePicture">
+      <div class="userAndContent">
+        <div class="user">
+          <p class="username">De Kale Kip</p>
+          <p class="tag">@DeKaleKip</p>
+          <p class="timePosted">&middot; 12-03-24</p>
+        </div>
+        <div class="content">
+          <p>lorem</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>bdgfbgfeht</p>
+        </div>
+        <div class="analytics">
+          <ul>
+            <li><a href="#">reactions 12</a></li>
+            <li><a href="#">shares 5</a></li>
+            <li><a href="#">likes 32</a></li>
+            <li><a href="#">views 103</a></li>
+            <li><a href="#">archive</a></li>
+          </ul>
+        </div>
+      </div>  
+    </article>
+
+    <article>
+      <img src="assets/img/chicken-solid-white.png" alt="profilePicture">
+      <div class="userAndContent">
+        <div class="user">
+          <p class="username">De Kale Kip</p>
+          <p class="tag">@DeKaleKip</p>
+          <p class="timePosted">&middot; 12-03-24</p>
+        </div>
+        <div class="content">
+          <p>lorem</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>bdgfbgfeht</p>
+        </div>
+        <div class="analytics">
+          <ul>
+            <li><a href="#">reactions 12</a></li>
+            <li><a href="#">shares 5</a></li>
+            <li><a href="#">likes 32</a></li>
+            <li><a href="#">views 103</a></li>
+            <li><a href="#">archive</a></li>
+          </ul>
+        </div>
+      </div>  
+    </article>
+
+    <article>
+      <img src="assets/img/chicken-solid-white.png" alt="profilePicture">
+      <div class="userAndContent">
+        <div class="user">
+          <p class="username">De Kale Kip</p>
+          <p class="tag">@DeKaleKip</p>
+          <p class="timePosted">&middot; 12-03-24</p>
+        </div>
+        <div class="content">
+          <p>lorem</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>bdgfbgfeht</p>
+        </div>
+        <div class="analytics">
+          <ul>
+            <li><a href="#">reactions 12</a></li>
+            <li><a href="#">shares 5</a></li>
+            <li><a href="#">likes 32</a></li>
+            <li><a href="#">views 103</a></li>
+            <li><a href="#">archive</a></li>
+          </ul>
+        </div>
+      </div>  
+    </article>
+
+    <article>
+      <img src="assets/img/chicken-solid-white.png" alt="profilePicture">
+      <div class="userAndContent">
+        <div class="user">
+          <p class="username">De Kale Kip</p>
+          <p class="tag">@DeKaleKip</p>
+          <p class="timePosted">&middot; 12-03-24</p>
+        </div>
+        <div class="content">
+          <p>lorem</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>bdgfbgfeht</p>
+        </div>
+        <div class="analytics">
+          <ul>
+            <li><a href="#">reactions 12</a></li>
+            <li><a href="#">shares 5</a></li>
+            <li><a href="#">likes 32</a></li>
+            <li><a href="#">views 103</a></li>
+            <li><a href="#">archive</a></li>
+          </ul>
+        </div>
+      </div>  
+    </article>
+
+    <article>
+      <img src="assets/img/chicken-solid-white.png" alt="profilePicture">
+      <div class="userAndContent">
+        <div class="user">
+          <p class="username">De Kale Kip</p>
+          <p class="tag">@DeKaleKip</p>
+          <p class="timePosted">&middot; 12-03-24</p>
+        </div>
+        <div class="content">
+          <p>lorem</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>bdgfbgfeht</p>
+        </div>
+        <div class="analytics">
+          <ul>
+            <li><a href="#">reactions 12</a></li>
+            <li><a href="#">shares 5</a></li>
+            <li><a href="#">likes 32</a></li>
+            <li><a href="#">views 103</a></li>
+            <li><a href="#">archive</a></li>
+          </ul>
+        </div>
+      </div>  
+    </article>
+
+    <article>
+      <img src="assets/img/chicken-solid-white.png" alt="profilePicture">
+      <div class="userAndContent">
+        <div class="user">
+          <p class="username">De Kale Kip</p>
+          <p class="tag">@DeKaleKip</p>
+          <p class="timePosted">&middot; 12-03-24</p>
+        </div>
+        <div class="content">
+          <p>lorem</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>bdgfbgfeht</p>
+        </div>
+        <div class="analytics">
+          <ul>
+            <li><a href="#">reactions 12</a></li>
+            <li><a href="#">shares 5</a></li>
+            <li><a href="#">likes 32</a></li>
+            <li><a href="#">views 103</a></li>
+            <li><a href="#">archive</a></li>
+          </ul>
+        </div>
+      </div>  
+    </article>
+
+    <article>
+      <img src="assets/img/chicken-solid-white.png" alt="profilePicture">
+      <div class="userAndContent">
+        <div class="user">
+          <p class="username">De Kale Kip</p>
+          <p class="tag">@DeKaleKip</p>
+          <p class="timePosted">&middot; 12-03-24</p>
+        </div>
+        <div class="content">
+          <p>lorem</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores corrupti at officiis est quam excepturi nostrum reiciendis totam omnis, sequi exercitationem, perferendis repellat animi ea ab tenetur dolor perspiciatis labore.</p>
+          <p>bdgfbgfeht</p>
+        </div>
+        <div class="analytics">
+          <ul>
+            <li><a href="#">reactions 12</a></li>
+            <li><a href="#">shares 5</a></li>
+            <li><a href="#">likes 32</a></li>
+            <li><a href="#">views 103</a></li>
+            <li><a href="#">archive</a></li>
+          </ul>
+        </div>
+      </div>  
+    </article>
+
+
+
+</div>
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <form action="loginaction.php" method="post">
             <label for="Email">Email</label>
-                <input type="email" name="Email" id="email" placeholder="Email" required >
-            <label for="Subject">Subject</label>
+                <input type="Email" name="Email" id="Email" placeholder="Email" required >
+            <label for="password">password</label>
                 <input type="password" name="password" id="password" placeholder="password">
             <input type="submit" name="submit" id="submit">
         </form>
