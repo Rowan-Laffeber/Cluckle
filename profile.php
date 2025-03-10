1eeb64122dffb89f4f2658dafb1aeec117ba5150
Tliefden <Tyreseliefden1@hotmail.com>
2025-03-10 13:07:11 +0100

    userpage mapje aangemaakt

---
+++ profile.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="description" content="The social media website Cluckle">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets/img/chicken-solid-white.png">
+    <title>Cluckle.com</title>
+    <link rel="stylesheet" href="assets/styles/main.css">
+    <!-- <link id="color_mode" rel="stylesheet" href="assets/cluckle-darkmode.css"> -->
+</head>
+<body>
+
+<div class="login">
+    <img src="assets/img/chicken-line-white.png" alt="">
+    <div class="loginSideText">
+        <h1>Whats Clucking</h1>
+        <form action="index.php" method="post"> <!-- loginaction.php -->
+            <label for="Email"></label>
+            <input type="Email" name="Email" id="Email" placeholder="Email" required >
+            <label for="password"></label>
+            <input type="password" name="password" id="password" placeholder="Password">
+            <input class="loginButton" type="submit" name="submit" id="submit" value="Log in">
+        </form>
+        <div class=linksLoginPage>
+            <a href="#">forgot Password</a>
+            <a href="#">create an account</a>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
