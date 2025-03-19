96750eab1cd58fbdb313c412871da7356bb0dd5d
Rowan Laffeber <9026051@student.zadkine.nl>
2025-03-19 08:42:42 +0100

    V0.0.1
    added get all for table

--- conn.php
+++ conn.php
@@ -14,3 +14,13 @@ try{
 catch (PDOException $foutmelding){    
     echo $foutmelding->getMessage();
 }
+
+$get_all_users = $conn->prepare("SELECT * FROM account");
+$get_all_users->execute();
+$users = $get_all_users->fetchAll();
+
+foreach ($users as $user){    
+    echo $user['name'];
+}
+
+
