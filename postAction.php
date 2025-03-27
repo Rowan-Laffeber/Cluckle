866e6bf4b18d5314fe54a710232994e0060a03dd
Rowan-Laffeber <9026051@student.zadkine.nl>
2025-03-27 19:23:34 +0100

    v0.0.0.1
    added userid to post

--- postAction.php
+++ postAction.php
@@ -1,11 +1,14 @@
 <?php 
 require "database/conn.php";
+require ("partials/session.php");
+
+$insert_user = $conn->prepare("INSERT INTO post (userId,contentText,datePosted,contentPic,contentVid) VALUES (:userId, :text, :date, :contentPic, :contentVid)");
+$insert_user->bindParam(":userId", $_POST['textarea']);
 
 
-$insert_user = $conn->prepare("INSERT INTO post (contentText,datePosted,contentPic,contentVid) VALUES (:text, :date, :contentPic, :contentVid)");
-$insert_user->bindParam(":text", $_POST['textarea']);
+$insert_user->bindParam(":text", $_SESSION['userId']);
 // $insert_user->bindParam(":contentPic", $_POST['contentPic']);
-$insert_user->bindParam(":contentVid", $_POST['contentVid']);
+// $insert_user->bindParam(":contentVid", $_POST['contentVid']);
 $insert_user->bindParam(":date", date("Y-m-d"));
 
 $insert_user->execute();
